f70d98965e2cc23976c7c61d31d77092697e5555
Stephan Preibisch <stephan.preibisch@gmx.de>
2020-05-29 08:34:46 -0400

    initialized bare Maven project with pom.xml set up

---
+++ src/main/java/net/preibisch/qualityest/Estimate_Quality.java
@@ -0,0 +1,15 @@
+package net.preibisch.qualityest;
+
+import ij.plugin.PlugIn;
+
+public class Estimate_Quality implements PlugIn
+{
+
+	@Override
+	public void run( String arg )
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
